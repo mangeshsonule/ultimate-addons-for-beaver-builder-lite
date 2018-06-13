57ab0df908dd37bc81453c943f920187e2503283
Mahesh Waghmare <mwaghmare7@gmail.com>
2018-06-13 10:59:34 +0530

    Removed go-premium notice.

--- includes/admin-settings-template-cloud.php
+++ includes/admin-settings-template-cloud.php
@@ -1,7 +1,5 @@
 <div id="fl-uabb-cloud-templates-form" class="fl-settings-form uabb-cloud-templates-fl-settings-form">
 
-	<div class="uabb-go-premium"><?php _e( '<a href="' . BB_ULTIMATE_ADDON_UPGRADE_URL . '" target="_blank">Go Premium</a> and get access to all Page Templates and Sections.', 'uabb' ); ?></div>
-
 	<form id="uabb-cloud-templates-form" action="<?php UABBBuilderAdminSettings::render_form_action( 'uabb-cloud-templates' ); ?>" method="post">
 
 		<?php if ( FLBuilderAdminSettings::multisite_support() && ! is_network_admin() ) : ?>
